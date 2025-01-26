50c59b6bbbcb53f30fc4b4b1ea2fb374bc0e9cfc
Felix Kaiser <felix-kaiser@gmx.de>
2025-01-26 12:58:54 +0100

    Finish challenge

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Happy developing ✨')
+const accordions = document.getElementsByClassName("accordion");
+
+for (const accordion of accordions) {
+    let accordionToggle = accordion.getElementsByClassName("accordion__toggle")[0];
+    let accordionContent = accordion.getElementsByClassName("accordion__content")[0];
+
+    accordionToggle.addEventListener("click", (event) => {
+        accordion.classList.toggle("accordion--open");
+        accordionContent.setAttribute("hidden", "true");
+
+        accordionToggle.setAttribute("aria-expanded", "false");
+
+        if(accordion.classList.contains("accordion--open")) {
+            accordionContent.removeAttribute("hidden");
+            accordionToggle.setAttribute("aria-expanded", "true");
+        }
+    })
+}
